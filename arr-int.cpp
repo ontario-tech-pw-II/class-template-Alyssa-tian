31391673d762d171cf2a8e57e359414a975ffada
Alyssa-tian <59973312+Alyssa-tian@users.noreply.github.com>
2020-03-12 12:29:06 -0400

    Add files via upload

--- arr-int.cpp
+++ arr-int.cpp
@@ -3,43 +3,47 @@
 #include <iostream>
 using namespace std;
 
+template <class T>
 class DynamicArray 
 {
 public:
   DynamicArray();
   ~DynamicArray();
-  void addEntry(int newEntry);
-  int getEntry(int index);
+  void addEntry(T newEntry);
+  T getEntry(int index);
   int getSize();
 
 private:
-  int *dynamicArray;
+  T *dynamicArray;
   int size;
 };
 
-DynamicArray::DynamicArray()
+template <class T>
+DynamicArray<T>::DynamicArray()
 {
   dynamicArray = nullptr;
   size = 0;
 }
 
-
-DynamicArray::~DynamicArray()
+template <class T>
+DynamicArray <T>::~DynamicArray()
 {
   if (dynamicArray != nullptr)
     delete[] dynamicArray;
 }
 
-int DynamicArray::getSize()
+template <class T>
+int DynamicArray<T>::getSize()
 {
   return size;
 }
 
 // Adds an entry to the dynamic array. 
-void DynamicArray::addEntry(int newEntry)
+template <class T>
+void DynamicArray<T>::addEntry(T newEntry)
 {
     // Create a new array, copy the contents of the old array, then delete it
-    int *newArray = new int[size + 1];
+    T *newArray = new T[size + 1];
     for (int i = 0; i < size; i++)
     {
     newArray[i] = dynamicArray[i];
@@ -53,10 +57,9 @@ void DynamicArray::addEntry(int newEntry)
     newArray[size++] = newEntry;
 }
 
-
-
+template <class T>
 // Retrieve the element at a given index
-int DynamicArray::getEntry(int index)
+T DynamicArray<T>::getEntry(int index)
 {
   if ((index < 0) || (index >= size)){
     cout << "Out of the index !" << endl;
@@ -66,7 +69,7 @@ int DynamicArray::getEntry(int index)
   return dynamicArray[index];
 }
 
-
+/*
 int main()
 {
   DynamicArray x;
@@ -86,4 +89,24 @@ int main()
   
   return 0;
 }
+*/
 
+int main()
+{
+  DynamicArray<string> x;
+
+  // Adding entries
+  x.addEntry("Frank");
+  x.addEntry("Wiggum");
+  x.addEntry("Nahasapeemapetilon");
+  x.addEntry("Quimby");
+  x.addEntry("Flanders");
+
+  // Output entries
+  cout << "List:" << endl;
+  
+  for (int i = 0; i < x.getSize(); i++)
+    cout << x.getEntry(i) << endl;
+  
+  return 0;
+}
